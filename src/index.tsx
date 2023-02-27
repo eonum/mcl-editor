5cda8328f08e09410b3c53b30518ea8299bacb22
Joel Bessire <joel.bessire@eonum.ch>
2023-02-27 16:10:28 +0100

    remove unnecessary comment

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 /**
  * Renders the whole application
  */
-// TODO: Use "createRoot" instead of "ReactDOM.render".
-//  This will enable React 18, but also leads to failed tests. Thus this maybe needs some more time...
 ReactDOM.render(
     // StrictMode renders components twice (on dev but not production), detecting problems / deprecations / warnings.
     <React.StrictMode>
