2e45d47123008d5ea3198bc28203c1cc8c52fe38
oli <oli.meier@bluewin.ch>
2023-05-10 23:11:23 +0200

    Added function tests

--- src/__test__/functions.test.js
+++ src/__test__/functions.test.js
@@ -1,5 +1,14 @@
+const extFunction = require('../Monaco/functions.js');
+
 // Default test - so this test suite doesn't fail
 test('default_test', () =>{
     expect(true).toBe(true)
 });
 
+test('arrayToString', () =>{
+    expect(extFunction.arrayToString(new Array("H", "E", "L", "L", "O")).toBe("HELLO"))
+});
+
+test('isCodeVariable', () =>{
+
+});
